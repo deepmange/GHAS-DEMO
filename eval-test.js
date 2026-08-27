7950b851de57c08c5a94de68dc9286c5e24c516b
deepmange <deep.mange@gmail.com>
2026-08-26 20:09:01 -0700

    Update eval-test.js with findings that CodeQL almost always catches

--- eval-test.js
+++ eval-test.js
@@ -1,2 +1,9 @@
-function executeUserInput(userInput){ eval(userInput); }
-executeUserInput("console.log(\'hello\')");
+const http = require("http");
+ 
+http.createServer((req, res) => {
+    const cmd = req.url.substring(1);
+  
+    require("child_process").exec(cmd);
+
+    res.end("done");
+}).listen(3000);
